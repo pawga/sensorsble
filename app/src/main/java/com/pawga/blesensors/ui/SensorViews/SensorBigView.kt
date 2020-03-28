bc5d48e675a861c3cd92e7b4e9b723e7d243f8fc
pawga <ivannikov.s@gmail.com>
2020-03-28 18:20:43 +0300

    debug component

--- app/src/main/java/com/pawga/blesensors/ui/SensorViews/SensorBigView.kt
+++ app/src/main/java/com/pawga/blesensors/ui/SensorViews/SensorBigView.kt
@@ -2,6 +2,7 @@ package com.pawga.blesensors.ui.SensorViews
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -12,32 +13,35 @@ import com.pawga.blesensors.R
 /**
  * Created by sivannikov
  */
+
 class SensorBigView(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+    private val type: TextView
+    private val value: TextView
+    private val unit: TextView
+    private val colorView: View
+    private val colorTextView: TextView
+    private val sensorAlarm: ImageView
+
     init {
         LinearLayout.inflate(context, R.layout.sensor_big_view, this)
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.SensorBigView)
-        try {
-            val imageView: ImageView = findViewById(R.id.image)
-            val textView: TextView = findViewById(R.id.caption)
 
-            val text = attributes.getString(R.styleable.SensorBigView_text)
-            val drawableId = attributes.getResourceId(R.styleable.SensorBigView_image, 0)
-            if (drawableId != 0) {
-                val drawable = AppCompatResources.getDrawable(context, drawableId)
-                image_thumb.setImageDrawable(drawable)
-            }
-            text_title.text = text
+        type = findViewById(R.id.sensorTypeTextView)
+        value = findViewById(R.id.sensorValueTextView)
+        unit = findViewById(R.id.sensorUnitTextView)
+        colorView = findViewById(R.id.sensorColorView)
+        colorTextView = findViewById(R.id.sensorColorTextView)
+        sensorAlarm = findViewById(R.id.sensorAlarmImageView)
+
+        try {
+            val typeText = attributes.getString(R.styleable.SensorBigView_type)
+            val valueText = attributes.getString(R.styleable.SensorBigView_value)
+            val unitText = attributes.getString(R.styleable.SensorBigView_unit)
+            type.text = typeText
+            value.text = valueText
+            unit.text = unitText
         } finally {
             attributes.recycle()
         }
-
-        val imageView: ImageView = findViewById(R.id.image)
-        val textView: TextView = findViewById(R.id.caption)
-
-        val attributeqs = context.obtainStyledAttributes(attrs, R.styleable.BenefitView)
-        imageView.setImageDrawable(attributes.getDrawable(R.styleable.BenefitView_image))
-        textView.text = attributes.getString(R.styleable.BenefitView_text)
-        attributes.recycle()
-
     }
 }
