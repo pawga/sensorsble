7510cc4ba134392d9f7232597c33bbcf1c2cc223
Pawga&Co <pawga777@gmail.com>
2020-04-15 22:03:05 +0300

    debug service refactoring

--- app/src/main/java/com/pawga/blesensors/MainActivity.kt
+++ app/src/main/java/com/pawga/blesensors/MainActivity.kt
@@ -29,9 +29,6 @@ class MainActivity : AppCompatActivity(), ThingySdkManager.ServiceConnectionList
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navController: NavController
-    private lateinit var thingySdkManager: ThingySdkManager
-    private var binder: ThingyBinder? = null
-    private var device: BluetoothDevice? = null
 
     private val bluetoothManager: BluetoothManager by inject()
 
@@ -44,44 +41,24 @@ class MainActivity : AppCompatActivity(), ThingySdkManager.ServiceConnectionList
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
-        thingySdkManager = ThingySdkManager.getInstance()
-
-        bluetoothManager.bluetoothDevice.observe(this, Observer {
-            if (it != null) {
-                device = it
-                thingySdkManager.connectToThingy(this, device, ThingyService::class.java)
-            } else if (device != null) {
-                thingySdkManager.disconnectFromAllThingies()
-            }
-        })
     }
 
     override fun onStart() {
         super.onStart()
-        thingySdkManager.bindService(this, ThingyService::class.java)
-        ThingyListenerHelper.registerThingyListener(this, thingyListener)
+        bluetoothManager.bindService(this, ThingyService::class.java)
     }
 
     override fun onStop() {
-        if (device != null) {
-            thingySdkManager.disconnectFromAllThingies()
-        }
-        thingySdkManager.unbindService(this);
+        bluetoothManager.unbindService(this);
         super.onStop()
     }
 
     override fun onServiceConnected() {
-        binder = thingySdkManager.getThingyBinder() as ThingyBinder
-        if (device != null && thingySdkManager.hasInitialServiceDiscoverCompleted(device)) {
-            onServiceDiscoveryCompletion(device!!)
-        }
+        bluetoothManager.onServiceConnected()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -104,167 +81,5 @@ class MainActivity : AppCompatActivity(), ThingySdkManager.ServiceConnectionList
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-
-    private fun onServiceDiscoveryCompletion(device: BluetoothDevice) {
-        thingySdkManager.enableEnvironmentNotifications(device, true)
-    }
-
-    private val thingyListener: ThingyListener = object : ThingyListener {
-        override fun onDeviceConnected(device: BluetoothDevice, connectionState: Int) {
-            Timber.d("onDeviceConnected")
-        }
-
-        override fun onDeviceDisconnected(device: BluetoothDevice, connectionState: Int) {
-            Timber.d("onDeviceDisconnected")
-        }
-
-        override fun onServiceDiscoveryCompleted(device: BluetoothDevice) {
-            onServiceDiscoveryCompletion(device)
-        }
-
-        override fun onBatteryLevelChanged(
-            bluetoothDevice: BluetoothDevice,
-            batteryLevel: Int
-        ) {
-        }
-
-        override fun onTemperatureValueChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            temperature: String
-        ) {
-            Timber.d("Temperature: $temperature")
-        }
-
-        override fun onPressureValueChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            pressure: String
-        ) {
-            Timber.d("Pressure: $pressure")
-        }
-
-        override fun onHumidityValueChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            humidity: String
-        ) {
-            Timber.d("Humidity: $humidity")
-        }
-
-        override fun onAirQualityValueChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            eco2: Int,
-            tvoc: Int
-        ) {
-            Timber.d("AirQuality: $eco2 - $tvoc")
-        }
-
-        override fun onColorIntensityValueChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            red: Float,
-            green: Float,
-            blue: Float,
-            alpha: Float
-        ) {
-            Timber.d("ColorIntensity: $alpha:$red:$green:$blue")
-        }
-
-        override fun onButtonStateChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            buttonState: Int
-        ) {
-        }
-
-        override fun onTapValueChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            direction: Int,
-            count: Int
-        ) {
-        }
-
-        override fun onOrientationValueChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            orientation: Int
-        ) {
-        }
-
-        override fun onQuaternionValueChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            w: Float,
-            x: Float,
-            y: Float,
-            z: Float
-        ) {
-        }
-
-        override fun onPedometerValueChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            steps: Int,
-            duration: Long
-        ) {
-        }
-
-        override fun onAccelerometerValueChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            x: Float,
-            y: Float,
-            z: Float
-        ) {
-        }
-
-        override fun onGyroscopeValueChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            x: Float,
-            y: Float,
-            z: Float
-        ) {
-        }
-
-        override fun onCompassValueChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            x: Float,
-            y: Float,
-            z: Float
-        ) {
-        }
-
-        override fun onEulerAngleChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            roll: Float,
-            pitch: Float,
-            yaw: Float
-        ) {
-        }
-
-        override fun onRotationMatrixValueChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            matrix: ByteArray
-        ) {
-        }
-
-        override fun onHeadingValueChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            heading: Float
-        ) {
-        }
-
-        override fun onGravityVectorChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            x: Float,
-            y: Float,
-            z: Float
-        ) {
-        }
-
-        override fun onSpeakerStatusValueChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            status: Int
-        ) {
-        }
-
-        override fun onMicrophoneValueChangedEvent(
-            bluetoothDevice: BluetoothDevice,
-            data: ByteArray
-        ) {
-        }
     }
 }
--- common/src/main/java/com/pawga/common/bluetooth/BluetoothManager.kt
+++ common/src/main/java/com/pawga/common/bluetooth/BluetoothManager.kt
@@ -1,12 +1,17 @@
 package com.pawga.common.bluetooth
 
 import android.bluetooth.BluetoothDevice
+import android.content.Context
 import android.os.Handler
 import android.os.ParcelUuid
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import no.nordicsemi.android.support.v18.scanner.*
+import no.nordicsemi.android.thingylib.BaseThingyService
+import no.nordicsemi.android.thingylib.ThingyListener
+import no.nordicsemi.android.thingylib.ThingyListenerHelper
+import no.nordicsemi.android.thingylib.ThingySdkManager
 import no.nordicsemi.android.thingylib.utils.ThingyUtils
 import timber.log.Timber
 import java.util.*
@@ -15,6 +20,11 @@ import java.util.*
  * Created by sivannikov
  */
 class BluetoothManager {
+
+    private lateinit var thingySdkManager: ThingySdkManager
+    private var device: BluetoothDevice? = null
+    private var binder: ThingyService.ThingyBinder? = null
+    private lateinit var activityContext: AppCompatActivity
 
     var isScanning = false
         private set
@@ -31,6 +41,32 @@ class BluetoothManager {
     val scanResults: LiveData<List<ScanResult>> = _scanResults
 
     val bluetoothDevice = MutableLiveData<BluetoothDevice?>()
+
+    init {
+        thingySdkManager = ThingySdkManager.getInstance()
+    }
+
+    fun bindService(context: AppCompatActivity, service: Class<out BaseThingyService?>?) {
+        activityContext = context
+        thingySdkManager.bindService(activityContext, service)
+        ThingyListenerHelper.registerThingyListener(activityContext, thingyListener)
+
+        bluetoothDevice.observe(activityContext, androidx.lifecycle.Observer {
+            if (it != null) {
+                device = it
+                thingySdkManager.connectToThingy(activityContext, device, ThingyService::class.java)
+            } else if (device != null) {
+                thingySdkManager.disconnectFromAllThingies()
+            }
+        })
+    }
+
+    fun unbindService(context: Context) {
+        if (device != null) {
+            thingySdkManager.disconnectFromAllThingies()
+        }
+        thingySdkManager.unbindService(activityContext);
+    }
 
     fun startScan() {
         if (isScanning) return
@@ -83,4 +119,173 @@ class BluetoothManager {
                 // should never be called
             }
         }
+
+    fun onServiceConnected() {
+        binder = thingySdkManager.getThingyBinder() as ThingyService.ThingyBinder
+        if (device != null && thingySdkManager.hasInitialServiceDiscoverCompleted(device)) {
+            onServiceDiscoveryCompletion(device!!)
+        }
+    }
+
+    private fun onServiceDiscoveryCompletion(device: BluetoothDevice) {
+        thingySdkManager.enableEnvironmentNotifications(device, true)
+    }
+
+    private val thingyListener: ThingyListener = object : ThingyListener {
+        override fun onDeviceConnected(device: BluetoothDevice, connectionState: Int) {
+            Timber.d("onDeviceConnected")
+        }
+
+        override fun onDeviceDisconnected(device: BluetoothDevice, connectionState: Int) {
+            Timber.d("onDeviceDisconnected")
+        }
+
+        override fun onServiceDiscoveryCompleted(device: BluetoothDevice) {
+            onServiceDiscoveryCompletion(device)
+        }
+
+        override fun onBatteryLevelChanged(
+            bluetoothDevice: BluetoothDevice,
+            batteryLevel: Int
+        ) {
+        }
+
+        override fun onTemperatureValueChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            temperature: String
+        ) {
+            Timber.d("Temperature: $temperature")
+        }
+
+        override fun onPressureValueChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            pressure: String
+        ) {
+            Timber.d("Pressure: $pressure")
+        }
+
+        override fun onHumidityValueChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            humidity: String
+        ) {
+            Timber.d("Humidity: $humidity")
+        }
+
+        override fun onAirQualityValueChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            eco2: Int,
+            tvoc: Int
+        ) {
+            Timber.d("AirQuality: $eco2 - $tvoc")
+        }
+
+        override fun onColorIntensityValueChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            red: Float,
+            green: Float,
+            blue: Float,
+            alpha: Float
+        ) {
+            Timber.d("ColorIntensity: $alpha:$red:$green:$blue")
+        }
+
+        override fun onButtonStateChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            buttonState: Int
+        ) {
+        }
+
+        override fun onTapValueChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            direction: Int,
+            count: Int
+        ) {
+        }
+
+        override fun onOrientationValueChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            orientation: Int
+        ) {
+        }
+
+        override fun onQuaternionValueChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            w: Float,
+            x: Float,
+            y: Float,
+            z: Float
+        ) {
+        }
+
+        override fun onPedometerValueChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            steps: Int,
+            duration: Long
+        ) {
+        }
+
+        override fun onAccelerometerValueChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            x: Float,
+            y: Float,
+            z: Float
+        ) {
+        }
+
+        override fun onGyroscopeValueChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            x: Float,
+            y: Float,
+            z: Float
+        ) {
+        }
+
+        override fun onCompassValueChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            x: Float,
+            y: Float,
+            z: Float
+        ) {
+        }
+
+        override fun onEulerAngleChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            roll: Float,
+            pitch: Float,
+            yaw: Float
+        ) {
+        }
+
+        override fun onRotationMatrixValueChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            matrix: ByteArray
+        ) {
+        }
+
+        override fun onHeadingValueChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            heading: Float
+        ) {
+        }
+
+        override fun onGravityVectorChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            x: Float,
+            y: Float,
+            z: Float
+        ) {
+        }
+
+        override fun onSpeakerStatusValueChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            status: Int
+        ) {
+        }
+
+        override fun onMicrophoneValueChangedEvent(
+            bluetoothDevice: BluetoothDevice,
+            data: ByteArray
+        ) {
+        }
+    }
 }
